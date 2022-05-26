be9e694a9db129af435ffa7620f80d33f417205b
Radu <zamfirr75@gmail.com>
2022-05-26 10:31:39 +0300

    i think it works

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import { Head, BlitzLayout } from "blitz"
 import { Suspense } from "react"
 import { UserInfo } from "app/pages"
-import "../../../styles/globlal.css"
+
 import Loader from "app/components/Loader"
 
 const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
@@ -23,7 +23,15 @@ const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
       >
         <UserInfo />
       </Suspense>
-      <Suspense fallback={<div className="ping"></div>}>{children}</Suspense>
+      <Suspense
+        fallback={
+          <div className="min-h-screen flex justify-center items-center">
+            <div className="ping"></div>
+          </div>
+        }
+      >
+        {children}
+      </Suspense>
     </>
   )
 }
