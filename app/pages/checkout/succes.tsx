1f2c1a908d4b864498ce95cbbc413cc5e5058038
Radu <zamfirr75@gmail.com>
2022-08-25 17:50:51 +0300

    Bugs

--- app/pages/checkout/succes.tsx
+++ app/pages/checkout/succes.tsx
@@ -1,4 +1,4 @@
-import { BlitzPage, useRouterQuery, invoke, useQuery } from "blitz"
+import { BlitzPage, useRouterQuery, invoke, useQuery, Router, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import confirmOrderPaid from "./mutations/confirmOrderPaid"
 import { QRCodeCanvas } from "qrcode.react"
@@ -13,6 +13,7 @@ const stripe = require("stripe")(
 const Succes: BlitzPage = () => {
   const ref = useRef()
   const query = useRouterQuery()
+  console.log(query)
 
   const test = invoke(confirmOrderPaid, query.session_id)
 
@@ -29,37 +30,41 @@ const Succes: BlitzPage = () => {
   }
   const booking = useQuery(getQrInfo, query.session_id)[0]
   console.log(booking)
-  return (
-    <>
-      <div>
-        <Rezumat booking={booking} />
-      </div>
-
-      <div className="flex justify-center items-center font-bold flex-col mt-20">
-        {
-          //@ts-ignore
-          <div ref={ref} onClick={downloadQrCode} className="cursor-pointer">
-            <QRCodeCanvas
-              id="qrCodeId"
-              size={300}
-              //@ts-ignore
-              value={query.session_id}
-              className="object-cover w-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
-            />
-          </div>
-        }
+  if (query.session_id) {
+    return (
+      <>
         <div>
-          <button
-            onClick={downloadQrCode}
-            type="button"
-            className="mt-10 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-          >
-            Descarca Biletul
-          </button>
+          <Rezumat booking={booking} />
         </div>
-      </div>
-    </>
-  )
+
+        <div className="flex justify-center items-center font-bold flex-col mt-20">
+          {
+            //@ts-ignore
+            <div ref={ref} onClick={downloadQrCode} className="cursor-pointer">
+              <QRCodeCanvas
+                id="qrCodeId"
+                size={300}
+                //@ts-ignore
+                value={query.session_id}
+                className="object-cover w-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out"
+              />
+            </div>
+          }
+          <div>
+            <button
+              onClick={downloadQrCode}
+              type="button"
+              className="mt-10 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            >
+              Descarca Biletul
+            </button>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  return <div></div>
 }
 export default Succes
 Succes.getLayout = (page) => <Layout title="Checkout">{page}</Layout>
