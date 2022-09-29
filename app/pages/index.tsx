4b8c2e3fb0262d17a407efc0612bc5d9c47c4021
Radu <zamfirr75@gmail.com>
2022-09-29 16:05:59 +0300

    test

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -38,7 +38,7 @@ const Home: BlitzPage = () => {
           <div
             className="block md:w-4/6 relative group min-h-screen "
             style={{
-              backgroundImage: "url(/Main.png)",
+              backgroundImage: "url(/Main-2.png)",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "center",
             }}
