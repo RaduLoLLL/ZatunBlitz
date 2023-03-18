a2c17bc70b10776c1410e1bbdae7d252ec0215a5
Gica <gicapetrescu@tutanota.com>
2023-03-18 17:02:06 +0200

    Fixed some bugs

--- app/pages/checkout/hooks/useCurrentQr.ts
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "blitz"
-import format from "date-fns/format"
-import getQrInfo from "../queries/getQrInfo"
-
-export const useCurrentQr = (orderId) => {
-  const [booking] = useQuery(getQrInfo, orderId) // Here I query the database
-  return booking
-}
