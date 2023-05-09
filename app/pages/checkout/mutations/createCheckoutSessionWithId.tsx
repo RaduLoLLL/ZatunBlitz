289102562990965a97cebfd36ed1faaf801df85f
radu <zamfirr75@gmail.com>
2023-05-09 19:53:46 +0300

    production

--- app/pages/checkout/mutations/createCheckoutSessionWithId.tsx
+++ app/pages/checkout/mutations/createCheckoutSessionWithId.tsx
@@ -12,14 +12,15 @@ export default async function createCheckoutSessionWithId({ booking_id, user }, 
   })
   if (!booking) return
 
-  const uuid = uuidv4().replace(/-/g, "")
   const bt_username = process.env.BT_USERNAME
   const bt_password = process.env.BT_PASSWORD
   const base_url = process.env.BASE_URL
   const bt_url = process.env.BT_URL
 
   const urlencodedPayload =
-    `userName=${bt_username}&password=${bt_password}&orderNumber=${uuid}&amount=${
+    `userName=${bt_username}&password=${bt_password}&orderNumber=${
+      booking.stripeSessionId
+    }&amount=${
       booking.total_price * 100
     }&currency=946&description=Plata%20rezervarii%20Balta%20Zatun&returnUrl=${base_url}%2Fcheckout%2Fsucces%3Fbooking_id%3D${
       booking.id
