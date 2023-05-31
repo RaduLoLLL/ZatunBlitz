ef007630ac20068a0edf8d6e1974fc768a26807c
radu <zamfirr75@gmail.com>
2023-05-31 20:00:38 +0300

    production

--- app/pages/dashboard/blocare-totala/queries/getLatestBlocked.tsx
+++ app/pages/dashboard/blocare-totala/queries/getLatestBlocked.tsx
@@ -2,5 +2,5 @@ import db from "db"
 
 //And this is the actual call to the database
 export default async function getLatestBlocked() {
-  return await db.blockedDates.findMany({ orderBy: { id: "desc" }, take: 1 })
+  return (await db.blockedDates.findMany({ orderBy: { id: "desc" }, take: 1 })) || false
 }
