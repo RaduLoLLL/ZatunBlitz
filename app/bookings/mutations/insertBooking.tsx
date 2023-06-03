934474dc9d9032a7e896a70851b01ea0936e39c9
Radu Zamfir <raduzamfir@Radus-MacBook-Pro.local>
2023-06-03 11:41:01 +0300

    production

--- app/bookings/mutations/insertBooking.tsx
+++ app/bookings/mutations/insertBooking.tsx
@@ -21,28 +21,30 @@ export default async function insertBooking(booking: booking, ctx: Ctx) {
 
   const spotsArray: any[] = []
   const casutaSpotsArray: any[] = []
-  bookings.map((booking) => {
-    if (booking.loc_pescuit.length) {
-      spotsArray.push(booking.loc_pescuit)
-    }
-    if (booking.casuta.length) {
-      casutaSpotsArray.push(booking.casuta)
-    }
-  })
-  spotsArray.push([1, 2, 9, 10])
-  const ocuppiedFishingSpots = [].concat.apply([], spotsArray)
-  const occupiedCasuta = [].concat.apply([], casutaSpotsArray)
+  if (bookings) {
+    bookings.map((booking) => {
+      if (booking.loc_pescuit.length) {
+        spotsArray.push(booking.loc_pescuit)
+      }
+      if (booking.casuta.length) {
+        casutaSpotsArray.push(booking.casuta)
+      }
+    })
+    spotsArray.push([1, 2, 9, 10])
+    const ocuppiedFishingSpots = [].concat.apply([], spotsArray)
+    const occupiedCasuta = [].concat.apply([], casutaSpotsArray)
 
-  booking.loc_pescuit?.map((loc) => {
-    if (ocuppiedFishingSpots.includes(loc)) {
-      throw { id: 1, loc: loc }
-    }
-  })
-  booking.casuta.map((loc) => {
-    if (occupiedCasuta.includes(loc)) {
-      throw { id: 2, loc }
-    }
-  })
+    booking.loc_pescuit?.map((loc) => {
+      if (ocuppiedFishingSpots.includes(loc)) {
+        throw { id: 1, loc: loc }
+      }
+    })
+    booking.casuta.map((loc) => {
+      if (occupiedCasuta.includes(loc)) {
+        throw { id: 2, loc }
+      }
+    })
+  }
 
   await db.booking
     .create({
