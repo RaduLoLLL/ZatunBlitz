759fcaf93bfda6bfa8b5b0be8e7dd105c3c3ed91
Radu Zamfir <raduzamfir@Radus-MacBook-Pro.local>
2023-06-03 11:52:15 +0300

    production

--- app/pages/dashboard/blocare-totala/queries/getLatestBlocked.tsx
+++ app/pages/dashboard/blocare-totala/queries/getLatestBlocked.tsx
@@ -2,5 +2,6 @@ import db from "db"
 
 //And this is the actual call to the database
 export default async function getLatestBlocked() {
-  return (await db.blockedDates.findMany({ orderBy: { id: "desc" }, take: 1 })) || false
+  const blocked = await db.blockedDates.findMany({ orderBy: { id: "desc" }, take: 1 })
+  return blocked ? blocked : []
 }
