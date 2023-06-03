5a2fcda8c70f86043e6008bd69ccb92f33863d0f
Radu Zamfir <raduzamfir@Radus-MacBook-Pro.local>
2023-06-03 11:38:07 +0300

    production

--- app/bookings/queries/getAllBookings.tsx
+++ app/bookings/queries/getAllBookings.tsx
@@ -3,9 +3,12 @@ import addDays from "date-fns/addDays"
 
 //And this is the actual call to the database
 export default async function getAllBookings(startsAt: string) {
-  return (
-    (await db.booking.findMany({
+  const bookings = await db.booking
+    .findMany({
       where: { starts_at: { gte: new Date(startsAt), lte: addDays(new Date(startsAt), 1) } },
-    })) || false
-  )
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  return bookings ? bookings : []
 }
