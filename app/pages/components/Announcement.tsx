42ee8d6ce4332624ad99c3dc3b302ba31e22faef
radu <zamfirr75@gmail.com>
2023-07-19 16:41:29 +0300

    production

--- app/pages/components/Announcement.tsx
+++ app/pages/components/Announcement.tsx
@@ -30,10 +30,10 @@ const Announcement = (props) => {
                 </p>
                 <hr className="my-3" />
                 <p className="text-lg font-medium text-gray-500">
-                  Pentru a rezerva foișoarele vă rugăm să sunați la numărul de telefon{" "}
+                  {/* Pentru a rezerva foișoarele vă rugăm să sunați la numărul de telefon{" "}
                   <a href="tel:0753104218">
                     <span className="font-bold text-red-600">0753 104 218</span>
-                  </a>
+                  </a> */}
                 </p>
                 <hr className="my-3" />
                 <p className="text-lg font-medium text-gray-500">
