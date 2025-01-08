783a9d53ebf56f4bd92397fff41f5b78a183c559
radu <zamfirr75@gmail.com>
2025-01-08 17:13:57 +0200

    User info updated

--- app/pages/contul-meu/queries/getUserInfo.tsx
+++
@@ -1,5 +0,0 @@
-import { Ctx } from "blitz"
-import db from "db"
-export default async function getUserInfo(_, ctx: Ctx) {
-  return await db.user.findUnique({ where: { userId: ctx.session.userId } })
-}
